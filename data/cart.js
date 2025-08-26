648bc668a5c5edbd64e051b5d7cdb6692c159a93
sudedonmezz <donmezsude41@gmail.com>
2025-08-26 17:57:29 +0300

    generate html and save the data in checkout.js

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,17 @@
-export const cart = [];
+export const cart = [
+{
+  productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  quantity:2
+},
+{
+  productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  quantity:1
+},
+{
+  productId:"19c6a64a-5463-4d45-9af8-e41140a4100c",
+  quantity:1
+}
+];
 
 
 export function addToCart(productId)

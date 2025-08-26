d453d3ec97802695d7c63a4753ad1c1147f3db63
sudedonmezz <donmezsude41@gmail.com>
2025-08-27 01:27:31 +0300

    formatCurrency function created and exported to other js files

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart, addToCart} from "/data/cart.js";
 import {products} from "/data/products.js";
+import { formatCurrency } from "./utils/money.js";
 let productHTML="";
 
 products.forEach((product)=> {
@@ -23,7 +24,7 @@ productHTML+=` <div class="product-container">
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
---
+++ scripts/utils/money.js
@@ -0,0 +1,3 @@
+export function formatCurrency(amount) {
+    return (amount/100).toFixed(2);
+ } 
